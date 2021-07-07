35fe639eb2dccea8dc89ef37ce9edd0f54d6801e
Lawrence Tam <lawrenceatam@gmail.com>
2021-07-06 22:26:25 -0700

    first commit

---
+++ todolist.js
@@ -0,0 +1,99 @@
+class Todo {
+  static DONE_MARKER = "X";
+  static UNDONE_MARKER = " ";
+
+  constructor(title) {
+    this.title = title;
+    this.done = false;
+  }
+
+  toString() {
+    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
+    return `[${marker}] ${this.title}`;
+  }
+
+  markDone() {
+    this.done = true;
+  }
+
+  markUndone() {
+    this.done = false;
+  }
+
+  isDone() {
+    return this.done;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+}
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  add(task) {
+    if (task instanceof Todo) {
+      this.todos.push(task);
+    } else {
+      throw new TypeError('can only add Todo objects');
+    }
+  }
+
+  size() {
+    return this.todos.length;
+  }
+
+  first() {
+    return this.todos[0];
+  }
+
+  last() {
+    return this.todos[this.size() - 1];
+  }
+
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+
+  isDone() {
+    return this.todos.every(todo => todo.isDone());
+  }
+
+  shift() {
+    return this.todos.shift();
+  }
+
+  pop() {
+    return this.todos.pop();
+  }
+
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1);
+  }
+
+  _validateIndex(index) {
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+
+  toString() {
+    let title = `---- ${this.title} ----`;
+    let list = this.todos.map(todo => todo.toString()).join('\n');
+    return `${title}\n${list}`;
+  }
+}
